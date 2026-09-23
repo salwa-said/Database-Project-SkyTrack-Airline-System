346d0ae3d44229e3520bf05e1b33208fe12e1fc0
salwa-said <salwwa2000@gmail.com>
2026-09-23 17:53:26 +0400

    Part 1: Insert Sample Data

--- TrackAirlineSQLQuery8.sql
+++ TrackAirlineSQLQuery8.sql
@@ -85,3 +85,45 @@ CREATE TABLE FlightCrew (
         ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+---SkyTrack Airline System – SQL Practice
+---Part 1: Insert Sample Data
+INSERT INTO Airport VALUES ('MCT','Muscat International','Muscat','Oman');
+INSERT INTO Airport VALUES ('DXB','Dubai International','Dubai','UAE');
+INSERT INTO Airport VALUES ('LHR','Heathrow','London','UK');
+INSERT INTO Airport VALUES ('JFK','John F. Kennedy','New York','USA');
+INSERT INTO Airport VALUES ('CDG','Charles de Gaulle','Paris','France');
+
+
+INSERT INTO Aircraft VALUES ('A123','A320','Airbus',2015,180);
+INSERT INTO Aircraft VALUES ('B456','737-800','Boeing',2012,160);
+INSERT INTO Aircraft VALUES ('C789','777-300','Boeing',2018,350);
+INSERT INTO Aircraft VALUES ('D321','A350','Airbus',2020,300);
+INSERT INTO Aircraft VALUES ('E654','Embraer E190','Embraer',2016,100);
+
+ALTER TABLE Flight
+DROP CONSTRAINT C2;
+
+
+ALTER TABLE Flight
+ALTER COLUMN Departure_datetime DATETIME NOT NULL;
+
+ALTER TABLE Flight
+ALTER COLUMN Arrival_datetime DATETIME NOT NULL;
+
+ALTER TABLE Flight
+ADD CONSTRAINT C2 CHECK (Arrival_datetime > Departure_datetime);
+
+select * from Flight
+select * from Airport
+select * from Aircraft
+
+
+INSERT INTO Flight VALUES ('SK101','2026-09-25 08:00:00','2026-09-25 12:00:00','Scheduled','MCT','DXB','A123');
+INSERT INTO Flight VALUES ('SK102','2026-09-26 09:00:00','2026-09-26 15:00:00','Delayed','DXB','LHR','B456');
+INSERT INTO Flight VALUES ('SK103','2026-09-27 07:30:00','2026-09-27 13:45:00','Cancelled','LHR','JFK','C789');
+INSERT INTO Flight VALUES ('SK104','2026-09-28 10:00:00','2026-09-28 18:00:00','Completed','JFK','CDG','D321');
+INSERT INTO Flight VALUES ('SK105','2026-09-29 06:00:00','2026-09-29 09:30:00','Scheduled','CDG','MCT','E654');
+INSERT INTO Flight VALUES ('SK106','2026-09-30 11:00:00','2026-09-30 16:00:00','Delayed','MCT','LHR','A123');
+INSERT INTO Flight VALUES ('SK107','2026-10-01 14:00:00','2026-10-01 20:00:00','Cancelled','DXB','JFK','B456');
+INSERT INTO Flight VALUES ('SK108','2026-10-02 05:00:00','2026-10-02 11:00:00','Completed','CDG','DXB','C789');
+
